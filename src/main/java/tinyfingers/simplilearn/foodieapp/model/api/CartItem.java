ad688b545e34df6715970e891b73f8882931e294
Linh Phan <linh.phan@partner.commerzbank.com>
2024-03-31 13:36:44 +0200

    Adapt api property

--- src/main/java/tinyfingers/simplilearn/foodieapp/model/api/CartItem.java
+++ src/main/java/tinyfingers/simplilearn/foodieapp/model/api/CartItem.java
@@ -1,5 +1,6 @@
 package tinyfingers.simplilearn.foodieapp.model.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,6 @@ public class CartItem {
   private Long id;
   private Integer quantity;
   private String name;
+  @JsonProperty("price")
   private double unitPrice;
 }
